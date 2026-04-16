4759b68738a97bddb29b47916672bb66c68ad6f9
Mohammad Hider Ahmad <mhider2006@gmail.com>
2026-04-15 22:08:38 -0400

    Seed database for all floorplan buildings and connect building pages to backend

--- app/seed/seed_data.py
+++ app/seed/seed_data.py
@@ -5,16 +5,27 @@ from app.models import Building, Reservation, Room, User
 
 
 BUILDINGS = [
-    {"code": "casl", "name": "College of Arts, Sciences, and Letters"},
+    {"code": "cb", "name": "College of Arts, Sciences, and Letters"},
+    {"code": "fhwc", "name": "Fieldhouse / Wellness Center"},
+    {"code": "ml", "name": "Mardigian Library"},
+    {"code": "elb", "name": "England Engineering Lab Building"},
+    {"code": "pec", "name": "Professional Education Center"},
+    {"code": "ruc", "name": "Renick University Center"},
+    {"code": "nsb", "name": "Natural Science Building"},
     {"code": "ssb", "name": "Social Sciences Building"},
     {"code": "cis", "name": "Computer & Information Science"},
-    {"code": "elb", "name": "England Engineering Lab Building"},
-    {"code": "nsb", "name": "Natural Science Building"},
-    {"code": "ml", "name": "Mardigian Library"},
+    {"code": "css", "name": "Campus Support Services"},
+    {"code": "iavs", "name": "Institute for Advanced Vehicle Systems"},
+    {"code": "nsbs", "name": "Natural Sciences Building South"},
+    {"code": "sfc", "name": "Science Faculty Center"},
+    {"code": "gb", "name": "Grounds Building"},
+    {"code": "slrc", "name": "Science Learning and Research Center"},
+    {"code": "hpec", "name": "Heinz Prechter Engineering Complex"},
+    {"code": "ca", "name": "CASL Annex"},
 ]
 
 ROOMS_BY_BUILDING = {
-    "casl": [
+    "cb": [
         {"room_number": "1000", "capacity": 40, "floor": "1"},
         {"room_number": "1001", "capacity": 35, "floor": "1"},
         {"room_number": "1013", "capacity": 30, "floor": "1"},
@@ -22,6 +33,52 @@ ROOMS_BY_BUILDING = {
         {"room_number": "2004", "capacity": 30, "floor": "2"},
         {"room_number": "3000", "capacity": 35, "floor": "3"},
         {"room_number": "3006", "capacity": 25, "floor": "3"},
+    ],
+    "fhwc": [
+        {"room_number": "1000", "capacity": 50, "floor": "GR"},
+        {"room_number": "1100", "capacity": 35, "floor": "1"},
+        {"room_number": "1200", "capacity": 30, "floor": "1"},
+        {"room_number": "2100", "capacity": 25, "floor": "2"},
+    ],
+    "ml": [
+        {"room_number": "2101", "capacity": 6, "floor": "2"},
+        {"room_number": "2102", "capacity": 6, "floor": "2"},
+        {"room_number": "2190", "capacity": 8, "floor": "2"},
+        {"room_number": "3101", "capacity": 6, "floor": "3"},
+    ],
+    "elb": [
+        {"room_number": "1000", "capacity": 40, "floor": "1"},
+        {"room_number": "1001", "capacity": 35, "floor": "1"},
+        {"room_number": "1003", "capacity": 30, "floor": "1"},
+        {"room_number": "1010", "capacity": 25, "floor": "1"},
+        {"room_number": "1021", "capacity": 25, "floor": "1"},
+        {"room_number": "2003", "capacity": 35, "floor": "2"},
+        {"room_number": "2008", "capacity": 35, "floor": "2"},
+        {"room_number": "2010", "capacity": 25, "floor": "2"},
+        {"room_number": "2034", "capacity": 25, "floor": "2"},
+        {"room_number": "3010", "capacity": 25, "floor": "3"},
+        {"room_number": "3026", "capacity": 25, "floor": "3"},
+        {"room_number": "3042", "capacity": 30, "floor": "3"},
+    ],
+    "pec": [
+        {"room_number": "1000", "capacity": 45, "floor": "1"},
+        {"room_number": "1010", "capacity": 30, "floor": "1"},
+        {"room_number": "1200", "capacity": 35, "floor": "1"},
+        {"room_number": "1410", "capacity": 25, "floor": "1"},
+        {"room_number": "2010", "capacity": 30, "floor": "2"},
+        {"room_number": "2030", "capacity": 25, "floor": "2"},
+    ],
+    "ruc": [
+        {"room_number": "100", "capacity": 80, "floor": "1"},
+        {"room_number": "120", "capacity": 40, "floor": "1"},
+        {"room_number": "200", "capacity": 60, "floor": "2"},
+        {"room_number": "220", "capacity": 25, "floor": "2"},
+    ],
+    "nsb": [
+        {"room_number": "1000", "capacity": 30, "floor": "1"},
+        {"room_number": "1002", "capacity": 25, "floor": "1"},
+        {"room_number": "1004", "capacity": 25, "floor": "1"},
+        {"room_number": "1006", "capacity": 25, "floor": "1"},
     ],
     "ssb": [
         {"room_number": "1000", "capacity": 100, "floor": "1"},
@@ -40,31 +97,46 @@ ROOMS_BY_BUILDING = {
         {"room_number": "203", "capacity": 20, "floor": "2"},
         {"room_number": "205", "capacity": 20, "floor": "2"},
     ],
-    "elb": [
+    "css": [
+        {"room_number": "101", "capacity": 20, "floor": "1"},
+        {"room_number": "102", "capacity": 15, "floor": "1"},
+    ],
+    "iavs": [
+        {"room_number": "100", "capacity": 30, "floor": "1"},
+        {"room_number": "110", "capacity": 25, "floor": "1"},
+        {"room_number": "200", "capacity": 25, "floor": "2"},
+        {"room_number": "210", "capacity": 20, "floor": "2"},
+    ],
+    "nsbs": [
+        {"room_number": "B10", "capacity": 20, "floor": "B"},
+        {"room_number": "100", "capacity": 30, "floor": "1"},
+        {"room_number": "200", "capacity": 25, "floor": "2"},
+        {"room_number": "210", "capacity": 20, "floor": "2"},
+    ],
+    "sfc": [
+        {"room_number": "B01", "capacity": 15, "floor": "B"},
+        {"room_number": "101", "capacity": 20, "floor": "1"},
+        {"room_number": "201", "capacity": 20, "floor": "2"},
+    ],
+    "gb": [
+        {"room_number": "101", "capacity": 10, "floor": "1"},
+    ],
+    "slrc": [
+        {"room_number": "101", "capacity": 35, "floor": "1"},
+        {"room_number": "201", "capacity": 30, "floor": "2"},
+        {"room_number": "301", "capacity": 30, "floor": "3"},
+        {"room_number": "401", "capacity": 25, "floor": "4"},
+    ],
+    "hpec": [
         {"room_number": "1000", "capacity": 40, "floor": "1"},
-        {"room_number": "1001", "capacity": 35, "floor": "1"},
-        {"room_number": "1003", "capacity": 30, "floor": "1"},
-        {"room_number": "1010", "capacity": 25, "floor": "1"},
-        {"room_number": "1021", "capacity": 25, "floor": "1"},
-        {"room_number": "2003", "capacity": 35, "floor": "2"},
-        {"room_number": "2008", "capacity": 35, "floor": "2"},
-        {"room_number": "2010", "capacity": 25, "floor": "2"},
-        {"room_number": "2034", "capacity": 25, "floor": "2"},
-        {"room_number": "3010", "capacity": 25, "floor": "3"},
-        {"room_number": "3026", "capacity": 25, "floor": "3"},
-        {"room_number": "3042", "capacity": 30, "floor": "3"},
+        {"room_number": "1100", "capacity": 35, "floor": "1"},
+        {"room_number": "2000", "capacity": 30, "floor": "2"},
+        {"room_number": "2100", "capacity": 25, "floor": "2"},
     ],
-    "nsb": [
-        {"room_number": "1000", "capacity": 30, "floor": "1"},
-        {"room_number": "1002", "capacity": 25, "floor": "1"},
-        {"room_number": "1004", "capacity": 25, "floor": "1"},
-        {"room_number": "1006", "capacity": 25, "floor": "1"},
-    ],
-    "ml": [
-        {"room_number": "2101", "capacity": 6, "floor": "2"},
-        {"room_number": "2102", "capacity": 6, "floor": "2"},
-        {"room_number": "2190", "capacity": 8, "floor": "2"},
-        {"room_number": "3101", "capacity": 6, "floor": "3"},
+    "ca": [
+        {"room_number": "100", "capacity": 20, "floor": "1"},
+        {"room_number": "110", "capacity": 15, "floor": "1"},
+        {"room_number": "200", "capacity": 20, "floor": "2"},
     ],
 }
 
